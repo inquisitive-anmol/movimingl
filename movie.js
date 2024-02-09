b1d87562d467cec2a2ca6b186d3a00bc87e47e0d
❤anmol❤ <131289076+inquisitive-anmol@users.noreply.github.com>
2024-02-09 20:31:47 +0530

    updated movie.js

---
+++ movie.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description : {
+    type: String, 
+    required: true
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  genre: { type: mongoose.Schema.Types.Array, required: true },
+  releaseDate: { type: Date, required: true },
+  posterLink: { type: String, required: true },
+  language: { type: String, required: true},
+},
+{timestamps: true});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
