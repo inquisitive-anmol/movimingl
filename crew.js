73f21f98c0bc66161a6fb74d4b02eac09397b08b
❤anmol❤ <131289076+inquisitive-anmol@users.noreply.github.com>
2024-02-09 20:09:22 +0530

    initial commit

---
+++ crew.js
@@ -0,0 +1,85 @@
+const movieCrewData = [
+    {
+      movie: movieDoc,
+      director: [
+        {
+          name: "Aditya Chopra",
+          imgUrl: "https://example.com/directors/aditya_chopra.jpg"
+        }
+      ],
+      producer: [
+        {
+          name: "Yash Chopra",
+          imgUrl: "https://example.com/producers/yash_chopra.jpg"
+        }
+      ],
+      cinematographer: [
+        {
+          name: "Manmohan Singh",
+          imgUrl: "https://example.com/cinematographers/manmohan_singh.jpg"
+        }
+      ],
+      musicArtist: [
+        {
+          name: "Jatin-Lalit",
+          imgUrl: "https://example.com/music_artists/jatin_lalit.jpg"
+        }
+      ]
+    },
+    {
+      movie: movieDoc,
+      director: [
+        {
+          name: "Rajkumar Hirani",
+          imgUrl: "https://example.com/directors/rajkumar_hirani.jpg"
+        }
+      ],
+      producer: [
+        {
+          name: "Vidhu Vinod Chopra",
+          imgUrl: "https://example.com/producers/vidhu_vinod_chopra.jpg"
+        }
+      ],
+      cinematographer: [
+        {
+          name: "C.K. Muraleedharan",
+          imgUrl: "https://example.com/cinematographers/ck_muraleedharan.jpg"
+        }
+      ],
+      musicArtist: [
+        {
+          name: "Shantanu Moitra",
+          imgUrl: "https://example.com/music_artists/shantanu_moitra.jpg"
+        }
+      ]
+    },
+    {
+      movie: movieDoc,
+      director: [
+        {
+          name: "Ashutosh Gowariker",
+          imgUrl: "https://example.com/directors/ashutosh_gowariker.jpg"
+        }
+      ],
+      producer: [
+        {
+          name: "Aamir Khan",
+          imgUrl: "https://example.com/producers/aamir_khan.jpg"
+        }
+      ],
+      cinematographer: [
+        {
+          name: "Anil Mehta",
+          imgUrl: "https://example.com/cinematographers/anil_mehta.jpg"
+        }
+      ],
+      musicArtist: [
+        {
+          name: "A.R. Rahman",
+          imgUrl: "https://example.com/music_artists/ar_rahman.jpg"
+        }
+      ]
+    },
+    // Add more movie crew data here
+  ];
+  
