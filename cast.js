be395fa83a2c83de14a8d6df025e63017bffa1d7
❤anmol❤ <131289076+inquisitive-anmol@users.noreply.github.com>
2024-02-09 20:00:16 +0530

    initial commit

---
+++ cast.js
@@ -0,0 +1,74 @@
+const movieCastData = [
+    {
+      movie: movieDoc,
+      actor: [
+        {
+          name: "Shah Rukh Khan",
+          imgUrl: "https://example.com/actors/shah_rukh_khan.jpg",
+          role: "Raj Malhotra"
+        },
+        
+      ],
+      actress: [
+        {
+        name: "Kajol",
+        imgUrl: "https://example.com/actors/kajol.jpg",
+        role: "Simran Singh"
+      },
+    ],
+      other: [
+        {
+        name:"lkdsfjldksf",
+        imgUrl: "jlsdakfjlsdkjfsdjf",
+        }
+      ],
+    },
+    {
+      movie: movieDoc,
+      actor: [
+        {
+          name: "Aamir Khan",
+          imgUrl: "https://example.com/actors/aamir_khan.jpg",
+          role: "Rancho"
+        },
+        
+      ],
+      actress: [
+        {
+            name: "R. Madhavan",
+            imgUrl: "https://example.com/actors/r_madhavan.jpg",
+            role: "Farhan Qureshi"
+          },
+      ],
+      other: [
+        {
+        name:"lkdsfjldksf",
+        imgUrl: "jlsdakfjlsdkjfsdjf",
+        },
+      ],
+    },
+    {
+      movie: movieDoc,
+      actor: [
+        {
+          name: "Aamir Khan",
+          imgUrl: "https://example.com/actors/aamir_khan.jpg",
+          role: "Bhuvan"
+        },
+      ],
+      actress: [
+        {
+            name: "lasdjflksda",
+            imgUrl: "ljsflsadfdf",
+            role: "jlsdkjflsdfj",
+        },
+      ],
+      other: [
+        {
+        name:"lkdsfjldksf",
+        imgUrl: "jlsdakfjlsdkjfsdjf",
+        },
+      ],
+    },
+
+];
